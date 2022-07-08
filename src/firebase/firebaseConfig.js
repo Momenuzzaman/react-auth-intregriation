fe27e6297bc44df0ce20dff9806fcc1debb96e71
mdemon420 <mdeamon4@gmail.com>
2022-07-08 17:06:54 +0600

    firebase add

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANYAjlUn02clEak-ksU3Wj0eN4Vl47uOc",
+    authDomain: "react-auth-intrgeation.firebaseapp.com",
+    projectId: "react-auth-intrgeation",
+    storageBucket: "react-auth-intrgeation.appspot.com",
+    messagingSenderId: "193050755238",
+    appId: "1:193050755238:web:f7bbda154470974bf3f467"
+};
+
+export default firebaseConfig;
